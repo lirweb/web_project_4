dc1df7a46e714a30375fbcfa800c874ec73f33d4
lirweb <88051331+lirweb@users.noreply.github.com>
2022-02-09 02:00:32 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let popupEdit = document.querySelector("popup__edit");
+let PopupOn = document.querySelector("popup");
+let PopupExit = document.querySelector("popup__exit");
+let popupName = document.querySelector("profile__name");
+let popupOccupation = document.querySelector("profile__occupation");
+
+
