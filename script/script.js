0a29f0c1579aa232d209411a9562b4d5372c3e57
lirweb <88051331+lirweb@users.noreply.github.com>
2022-02-20 19:06:25 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,35 @@
+const popup = document.querySelector(".popup");
+const popupOn = document.querySelector(".popup__on");
+const profile = document.querySelector('.profile')
+const profileName = document.querySelector(".profile__name-text");
+const profileOccupation = document.querySelector(".profile__occupation");
+const editButton = document.querySelector(".button__edit");
+const profileInfo = document.querySelector(".profile__info")
+const inputName = document.querySelector(".popup__input_type_name");
+const inputOccupation = document.querySelector(".popup__input_type_occupation");
+const buttonSubmit = document.querySelector(".button__submit")
+const buttonExit = document.querySelector(".button__exit")
+const buttonLike = document.querySelector(".button__like ");
+const profilePopup = document.querySelector(".popup_input_type_name");
+const profileForm = document.querySelector(".popup__box");
+
+function openForm() {
+    inputName.value = profileName.textContent;
+    inputOccupation.value = profileOccupation.textContent;
+    popup.classList.add("popup_on");
+}
+
+function popupOut() {
+    popup.classList.remove("popup_on");
+}
+
+function submitForm(event) {
+    profileName.textContent = inputName.value;
+    profileOccupation.textContent = inputOccupation.value;
+    event.preventDefault();
+    popupOut();
+}
+
+form.addEventListener("submit", submitForm);
+buttonExit.addEventListener("click", popupOut);
+editButton.addEventListener("click", openForm);
